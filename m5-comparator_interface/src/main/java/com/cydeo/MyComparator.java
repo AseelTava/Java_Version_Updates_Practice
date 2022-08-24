6ed8c52d1e8155c48747a0c3e09d8bdbcf2f7a86
casho <aseelhussein@aol.com>
2022-08-23 22:15:19 -0400

    CompareTO Practice and Tasks

---
+++ m5-comparator_interface/src/main/java/com/cydeo/MyComparator.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<Integer> {
+
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return 0;
+    }
+}
+
